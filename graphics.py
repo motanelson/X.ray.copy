b8a366439a3ad8725f252ed489747dd399c2df06
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-05-30 06:55:54 +0100

    Add files via upload

--- graphics.py
+++ graphics.py
@@ -7,21 +7,32 @@ import tkinter as tk
 
 
 class myapps:
-    def __init__(self,root:tk.Tk):
+    def __init__(self,root:tk.Tk,backgrounds:str,widths:int,heights:int,colors:str,steps:int,tiltes:str):
         self.root=root
-        self.root.title("graphics....")
-        self.root.geometry("640x480")
-        self.root.configure(background="black")
-        self.canvas=tk.Canvas(background="black",width=640,height=480)
+        self.root.title(tiltes)
+        self.root.geometry(str(widths)+"x"+str(heights))
+        self.root.configure(background=backgrounds)
+        self.canvas=tk.Canvas(background=backgrounds,width=widths,height=heights)
         self.canvas.pack(padx=1,pady=1)
         c = self.canvas
         c.delete("all")
-        for a in range(0,480,20):
-            c.create_line(0,a,640,a,fill="white")
+        for a in range(0,heights,steps):
+            c.create_line(0,a,widths,a,fill=colors)
         
-        for a in range(0,640,20):
-            c.create_line(a,0,a,480,fill="white")
+        for a in range(0,widths,steps):
+            c.create_line(a,0,a,heights,fill=colors)
 
-root=tk.Tk()
-apps=myapps(root)
-root.mainloop()
+
+def starts(backgrounds:str,widths:int,heights:int,colors:str,steps:int,tiltes:str):
+    root=tk.Tk()
+    apps=myapps(root,backgrounds,widths,heights,colors,steps,tiltes)
+    root.mainloop()
+
+backgrounds:str="black"
+colors:str="white"
+widths:int=640
+heights:str=480
+steps:int=20
+tiltes:str="grid ..........."
+
+starts(backgrounds,widths,heights,colors,steps,tiltes)
